cc63d410c09b1d6f221a7b9e917565815be66723
Laxman <88457639+Laxman67@users.noreply.github.com>
2024-10-11 09:36:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          'http://localhost:4000/api/v1/user/admin/me',
+          `${import.meta.env.VITE_BACKEND_URL}/user/admin/me`,
           {
             withCredentials: true,
           }
